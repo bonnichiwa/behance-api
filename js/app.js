13e7b31102130c1f1740775a28889485e359254d
bonnichiwa <bonnie.pham@ryerson.ca>
2015-08-31 11:59:17 -0400

    added getUsers function

--- js/app.js
+++ js/app.js
@@ -3,19 +3,52 @@ $(document).ready(function(){
     $('.results').html('');
     var area = $(this).find("input[name='area']").val();
     getUsers(area);
-  });
+  })
+
+  $.get("http://ipinfo.io", function (response) {
+    $("#address").html("Location: " + response.city + ", " + response.region);
+}, "jsonp");
+
 });
 
+
+// --- Error Message --- //
+var showError = function(error){
+  var errorElem = $('.templates .error').clone();
+  var errorText = '<p>' + error + '</p>';
+  errorElem.append(errorText);
+};
+
+// --- Getting Users --- //
+
 var getUsers = function(area) {
-  var request = {q: area,
+  var request = {city: area,
     site: 'behance',
-    sort: 'appreciations'
+    sort: 'appreciations',
+    client_id: 'g9hggzThToiGPThHB94XNUhzwkALb8N5'
   };
 
   var result = $.ajax({
-    url: "https://api.behance.net/v2/users?q=matias&client_id=1234567890"
+    url: "http://www.behance.net/v2/users?" + request.client_id + "&sort=" + request.sort + "&city=" request.city,
+    data: request,
+    dataType: "jsonp"
+    type: "GET",
   })
-}
+  .done(function(result) {
+    var searchResults = showSearchResults(request.city, result.users.length);
+
+    $('.search-results').html(searchResults);
+
+    $.each(result.users, function(i, item){
+      var user = showUser(item);
+      $('.results').append(user);
+    });
+  })
+  .fail(function(jqXHR, error, errorThrown) {
+    var errorElem = showError(error);
+    $('.search-results').append(errorElem);
+  });
+};
 
 
 // require(['be'], function(be) {
