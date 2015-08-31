b8f1c7f14c56cf463568bc6d6a94b637b675a330
bonnichiwa <bonnie.pham@ryerson.ca>
2015-08-31 12:48:44 -0400

    function not printing out results

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 $(document).ready(function(){
-  $('#search-area').submit(function(event){
+  $('.search-area').submit(function(event){
     $('.results').html('');
     var area = $(this).find("input[name='area']").val();
     getUsers(area);
+    console.log("search button pressed");
   })
 
   $.get("http://ipinfo.io", function (response) {
     $("#address").html("Location: " + response.city + ", " + response.region);
+    console.log("Current location: " + response.city);
 }, "jsonp");
+    
 
 });
 
@@ -20,53 +23,42 @@ var showError = function(error){
 };
 
 // --- Getting Users --- //
+var showUsers = function(user) {
+  console.log("Reached showUsers function");
+
+  var result = $('.templates .search').clone();
+
+  var displayUser = result.find('.username a');
+  displayUser.attr('href', user.url);
+  displayUser.text(user.display_name);
+}
 
 var getUsers = function(area) {
-  var request = {city: area,
-    site: 'behance',
+  console.log("reached getUsers");
+  var request = {
+    city: area,
     sort: 'appreciations',
     client_id: 'g9hggzThToiGPThHB94XNUhzwkALb8N5'
   };
 
   var result = $.ajax({
-    url: "http://www.behance.net/v2/users?" + request.client_id + "&sort=" + request.sort + "&city=" request.city,
+    url: "http://www.behance.net/v2/users?client_id=" + request.client_id + "&sort=" + request.sort + "&city=" + request.city,
     data: request,
     dataType: "jsonp"
-    type: "GET",
   })
   .done(function(result) {
-    var searchResults = showSearchResults(request.city, result.users.length);
-
-    $('.search-results').html(searchResults);
+    console.log("Found results");
 
     $.each(result.users, function(i, item){
-      var user = showUser(item);
+      console.log(result.users);
+      var user = showUsers(item);
       $('.results').append(user);
     });
+
+    console.log("Appended results");
   })
   .fail(function(jqXHR, error, errorThrown) {
     var errorElem = showError(error);
     $('.search-results').append(errorElem);
   });
 };
-
-
-// require(['be'], function(be) {
-// be('g9hggzThToiGPThHB94XNUhzwkALb8N5');
-// });
-
-// require(['be'], function(be) {
-//  // Using promises
-//  be('BehanceApiKey')
-//  .project.search('cats')
-//  .then(function success(results) {
-//    console.log(results);
-//  }, function failure(error) {
-//    console.error(error);
-//  });
-
-//  // Using callbacks
-//  be.project.search('dogs', function success(results) {
-//    console.log(results);
-//  });
-// });
